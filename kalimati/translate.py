3e2851207e84101484d6fc6410e9a7f97ab69887
raichu69 <anshanpool@gmail.com>
2023-03-24 12:04:34 +0545

    Added a script to translate price from nepali to english

---
+++ kalimati/translate.py
@@ -0,0 +1,39 @@
+# importing libraries
+import csv
+import googletrans
+from googletrans import *
+
+# making the instance og google trnaslator class 
+translator = googletrans.Translator()
+
+with open('scrapedData/3-24.csv') as file:
+    lines = csv.reader(file, delimiter=',')
+    line_length = 0
+    for line in lines:
+
+        if line_length == 0:
+           line_length += 1
+           with open('cleanData/2023-3-24.csv',mode='w') as tfile:
+               writer = csv.writer(tfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+               writer.writerow(line)
+
+        # if line_length <= 80:
+        #     line_length +=1
+
+
+        else:
+
+            minprice = translator.translate(line[1], dest='en',src='nepali').text
+            maxprice = translator.translate(line[2], dest='en',src='nepali').text
+            avgprice = translator.translate(line[3], dest='en',src='nepali').text
+
+            # print(line[0],minprice, maxprice, avgprice)
+            # break
+
+
+            with open('cleanData/2023-3-24.csv',mode='a') as tfile:
+                writer = csv.writer(tfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([line[0],minprice,maxprice,avgprice])
+
+
+            
