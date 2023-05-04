2aa2211a8069b99550045b686d5765827cc965fa
Ansan Rai <anshanpool@gmail.com>
2023-05-04 18:48:05 +0545

    before update

--- kalimati/translate.py
+++ kalimati/translate.py
@@ -1,3 +1,5 @@
+
+
 # importing libraries
 import csv
 import googletrans
@@ -6,14 +8,14 @@ from googletrans import *
 # making the instance og google trnaslator class 
 translator = googletrans.Translator()
 
-with open('scrapedData/5-02.csv') as file:
+with open('scrapedData/5-04.csv') as file:
     lines = csv.reader(file, delimiter=',')
     line_length = 0
     for line in lines:
 
         if line_length == 0:
            line_length += 1
-           with open('cleanData/2023-5-02.csv',mode='w') as tfile:
+           with open('cleanData/2023-5-04.csv',mode='w') as tfile:
                writer = csv.writer(tfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                writer.writerow(line)
 
@@ -31,7 +33,7 @@ with open('scrapedData/5-02.csv') as file:
             # break
 
 
-            with open('cleanData/2023-5-02.csv',mode='a') as tfile:
+            with open('cleanData/2023-5-04.csv',mode='a') as tfile:
                 writer = csv.writer(tfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 writer.writerow([line[0],minprice,maxprice,avgprice])
 
